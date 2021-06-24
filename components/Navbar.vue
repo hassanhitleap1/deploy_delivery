d893577c1d7d7bbc3dd979169ddb90ee37108be9
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-24 17:13:26 +0300

    commpnunt:

---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+<!--    <ul class="navbar-nav">-->
+<!--      <li class="nav-item">-->
+<!--        <NuxtLink class="nav-link" data-widget="pushmenu" to="/">Home page <i class="fas fa-bars"></i></NuxtLink>-->
+<!--&lt;!&ndash;        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>&ndash;&gt;-->
+<!--      </li>-->
+<!--      <li class="nav-item d-none d-sm-inline-block">-->
+<!--        <a href="index3.html" class="nav-link">Home</a>-->
+<!--      </li>-->
+<!--      <li class="nav-item d-none d-sm-inline-block">-->
+<!--        <NuxtLink class="nav-link" data-widget="pushmenu" to="/">Contact</NuxtLink>-->
+<!--      </li>-->
+<!--    </ul>-->
+
+    <!-- Right navbar links -->
+<!--    <MessagesDropdown />-->
+  </nav>
+  <!-- /.navbar -->
+</template>
+
+<script>
+    import MessagesDropdown from "./MessagesDropdown";
+    export default {
+        name: "Navbar",
+        components: {MessagesDropdown}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,25 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script:[
+        {type: "text/javascript",src:"~/assets/plugins/jquery/jquery.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/jquery-ui/jquery-ui.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/jquery-ui/jquery-ui.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/chart.js/Chart.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/sparklines/sparkline.js"},
+        {type: "text/javascript",src:"~/assets/plugins/jqvmap/jquery.vmap.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"},
+        {type: "text/javascript",src:"~/assets/plugins/jquery-knob/jquery.knob.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/moment/moment.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/daterangepicker/daterangepicker.js"},
+        {type: "text/javascript",src:"~/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/summernote/summernote-bs4.min.js"},
+        {type: "text/javascript",src:"~/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"},
+        {type: "text/javascript",src:"~/assets/dist/js/adminlte.js"},
+        {type: "text/javascript",src:"~/assets/dist/js/demo.js"},
+        {type: "text/javascript",src:"~/assets/dist/js/pages/dashboard.js"}
     ]
   },
   /*
@@ -23,7 +42,18 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/plugins/fontawesome-free/css/all.min.css',
+    // 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+    '~/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+    '~/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+    '~/assets/plugins/jqvmap/jqvmap.min.css',
+    '~/assets/dist/css/adminlte.min.css',
+    '~/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+    '~/assets/plugins/daterangepicker/daterangepicker.css',
+    '~/assets/plugins/summernote/summernote-bs4.min.css'
   ],
+
+
   /*
   ** Plugins to load before mounting the App
   */
