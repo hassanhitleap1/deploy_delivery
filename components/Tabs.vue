b6690d7ed853e8d161ac336b763c117fe56085e6
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-05 12:43:27 +0300

    26

---
+++ components/Tabs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+      <a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+      <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+      <a class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+    </div>
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+        <p>clicked home</p>
+        <p>clicked home</p>
+        <p>clicked home</p>
+      </div>
+      <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+        <p>clicked profile</p>
+        <p>clicked profile</p>
+        <p>clicked profile</p>
+      </div>
+      <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+        <p>clicked contact</p>
+        <p>clicked contact</p>
+        <p>clicked contact</p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Tabs"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="wrapper">
-    <LeftNavBar2 />
-    <nuxt />
+    <div class="container-fluid">
+      <div class="row">
+         <Header />
+        <LeftNavBar />
+        <div class="col-sm p-3 min-vh-100">
+          <!-- content -->
+            <nuxt />
+          <!-- end  content -->
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
     import LeftNavBar from "../components/LeftNavBar";
     import LeftNavBar2 from "../components/LeftNavBar2";
+    import Header from "../components/Header";
     export default {
-        components: {LeftNavBar2, LeftNavBar}
+        components: {Header, LeftNavBar2, LeftNavBar}
     }
 </script>
