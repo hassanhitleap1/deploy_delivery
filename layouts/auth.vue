07cfa495c90a4f53ed943244206b092c03531d51
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-09 16:30:46 +0300

    29

---
+++ layouts/auth.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-sm p-3 min-vh-100">
+          <!-- content -->
+            <nuxt />
+          <!-- end  content -->
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    import LeftNavBar from "../components/LeftNavBar";
+    import LeftNavBar2 from "../components/LeftNavBar2";
+    import Header from "../components/Header";
+    export default {
+        components: {Header, LeftNavBar2, LeftNavBar}
+    }
+</script>
